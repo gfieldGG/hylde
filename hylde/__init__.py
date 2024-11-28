61eb346eeb13a7452e11a2a731d9afd6c82e0a11
MAHF <github@gfield.de>
2024-11-28 15:27:56 +0100

    config file improvements

--- hylde/__init__.py
+++ hylde/__init__.py
@@ -3,7 +3,9 @@ from loguru import logger as lolg
 from dynaconf import Dynaconf  # type:ignore
 
 # get settings
-settings = Dynaconf(envvar_prefix="", settings_files=[Path("./config.toml").resolve()])
+settings = Dynaconf(
+    envvar_prefix="HYLDE", settings_files=["config.toml", "config.dev.toml"]
+)
 
 # configure Loguru
 lolg.add(settings.logfile, rotation="1 MB", retention="7 days", level="DEBUG")
