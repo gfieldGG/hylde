3d2d2d8e62d063aefe219e705741fc9572f01bdf
MAHF <github@gfield.de>
2025-07-20 23:38:59 +0200

    add basic error recovery for gallerydl

--- hylde/downloaders/gallerydl.py
+++ hylde/downloaders/gallerydl.py
@@ -1,3 +1,4 @@
+import shutil
 import tempfile
 from pathlib import Path
 
@@ -9,7 +10,6 @@ from hylde import lolg, settings
 
 output_dir = Path(tempfile.gettempdir()) / "hylde" / "gallerydl"  # TODO expose setting
 
-gdl.config.load()  # load default config files
 gdl.config.set(("extractor",), "base-directory", output_dir.as_posix())
 
 
@@ -40,8 +40,24 @@ class GoodJob(gdl.job.DownloadJob):
 
 def download_url(url: str, url_key: str) -> list[Path] | None:
     """Download file for url. Return full file paths. Return empty list on retryable problems. Return None if download failed."""
+    gdl.config.set(("extractor",), "directory", [url_key])
     fc = FileCollector()
     job = GoodJob(url)
     job.register_hooks(hooks={"file": fc.filepath_hook})
     job.run()
+
+    if not fc.files:
+        directory = output_dir / url_key
+        if directory.exists() and any(directory.iterdir()):
+            lolg.warning(
+                f"gallerydl returned no filepaths for '{url_key}' but '{directory}' contained files."
+            )
+            lolg.warning(f"Deleting files in '{directory}' to allow retry...")
+            shutil.rmtree(directory)
+        else:
+            lolg.error(
+                f"gallerydl returned no filepaths for '{url_key}' and '{directory}' was empty."
+            )
+            return None
+
     return fc.files
